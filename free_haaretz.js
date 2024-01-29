a195d7db917a6740cd84adc8d45453206be30022
lironzs <92261863+lironzs@users.noreply.github.com>
2024-01-29 14:31:34 +0200

    Update free_haaretz.js

--- free_haaretz.js
+++ free_haaretz.js
@@ -1,8 +1,8 @@
 
 // Reference: https://support.google.com/webmasters/answer/1061943?hl=en
 var USER_AGENTS = {
-  DESKTOP: 'Googlebot/2.1 (+http://www.googlebot.com/bot.html)',
-  MOBILE: 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+  DESKTOP: 'Mozilla/5.0 (compatible; SemrushBot; +http://www.semrush.com/bot.html)',
+  MOBILE: 'Mozilla/5.0 (compatible; SemrushBot; +http://www.semrush.com/bot.html) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
 };
 
 chrome.webRequest.onBeforeRequest.addListener(
